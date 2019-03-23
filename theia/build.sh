093ee760ae547319fa95150df2bc5eae0290cfff
502647092 <admin@yumc.pw>
2019-03-23 23:27:35 +0800

    feat: update kodexplorer

--- theia/build.sh
+++ theia/build.sh
@@ -1,1 +1,1 @@
-proxychains4 docker build . -f Dockerfile-full --build-arg HTTPS_PROXY=http://192.168.0.2:8118 --network host
+proxychains4 docker build . -f Dockerfile-full -t miaowoo/theia --build-arg HTTPS_PROXY=http://192.168.0.2:8118 --network host
