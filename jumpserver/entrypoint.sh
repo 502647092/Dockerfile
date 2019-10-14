c3592ebbcb3148a7e1b306597e1317b7946fe125
MiaoWoo <admin@yumc.pw>
2019-10-14 20:33:27 +0800

    feat: add koko
    Signed-off-by: MiaoWoo <admin@yumc.pw>

--- jumpserver/entrypoint.sh
+++ jumpserver/entrypoint.sh
@@ -32,13 +32,18 @@ if [ ! -f "/opt/jumpserver/config.yml" ]; then
     sed -i "s/# REDIS_PASSWORD: /REDIS_PASSWORD: $REDIS_PASSWORD/g" /opt/jumpserver/config.yml
 fi
 
-if [ -f "/etc/nginx/nginx.conf" ]; then
-    sed -i "s/127.0.0.1:2223/koko:2222/g" /etc/nginx/nginx.conf
-    sed -i "s/proxy_pass cocossh/proxy_pass kokossh/g" /etc/nginx/nginx.conf
+if [ ! -f "/opt/koko/config.yml" ]; then
+    cp /opt/koko/config_example.yml /opt/koko/config.yml
+    sed -i "s/BOOTSTRAP_TOKEN: <PleasgeChangeSameWithJumpserver>/BOOTSTRAP_TOKEN: $BOOTSTRAP_TOKEN/g" /opt/coco/config.yml
+    sed -i "s/# BIND_HOST: 0.0.0.0/BIND_HOST: 127.0.0.1/g" /opt/coco/config.yml
+    sed -i "s/# SSHD_PORT: 2222/SSHD_PORT: 2223/g" /opt/coco/config.yml
+    sed -i "s/# LOG_LEVEL: INFO/LOG_LEVEL: ERROR/g" /opt/coco/config.yml
+    sed -i "s@# SFTP_ROOT: /tmp@SFTP_ROOT: /@g" /opt/coco/config.yml
+    echo "ENABLE_PROXY_PROTOCOL: true" >> /opt/coco/config.yml
 fi
 
 if [ -f "/etc/nginx/nginx.conf" ]; then
-    sed -i "s/localhost:5000/koko:5000/g" /etc/nginx/nginx.conf
+    sed -i "s/proxy_pass cocossh/proxy_pass kokossh/g" /etc/nginx/nginx.conf
 fi
 
 source /opt/py3/bin/activate
@@ -53,6 +58,12 @@ sleep 20
 echo "Starting gunicorn..."
 ./jms start gunicorn &
 sleep 30
+while [ "$(curl -I -m 10 -o /dev/null -s -w %{http_code} 127.0.0.1:8080)" != "302" ]
+do
+    echo "wait for gunicorn ready"
+    sleep 2
+done
+cd /opt/koko && ./koko &
 /etc/init.d/guacd start
 sh /config/tomcat9/bin/startup.sh
 /usr/sbin/nginx &
