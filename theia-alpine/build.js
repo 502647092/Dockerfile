190cfdb28bdebe51a31ead181dd796667bd31f75
MiaoWoo <admin@yumc.pw>
2019-12-17 12:23:30 +0800

    feat: add build-in plugin support
    Signed-off-by: MiaoWoo <admin@yumc.pw>

--- theia-alpine/build.js
+++ theia-alpine/build.js
@@ -3,11 +3,15 @@ var process = require('child_process');
 
 var package = JSON.parse(fs.readFileSync('package.json').toString())
 for (adapter in package.adapters) {
-    var args = adapter.split('.')
-    var publisher = args[0]
-    var asset = args[1]
-    var varsion = process.execSync('./build.sh ' + adapter).toString().replace('\n', '')
-    package.adapters[adapter] = `https://ms-vscode.gallery.vsassets.io/_apis/public/gallery/publisher/${publisher}/extension/${asset}/${varsion}/assetbyname/Microsoft.VisualStudio.Services.VSIXPackage`
+    if (adapter.startsWith('@theia/vscode-builtin')) {
+        package.adapters[adapter] = process.execSync(`npm info ${adapter}@next dist.tarball`).toString().replace('\n', '')
+    } else {
+        var args = adapter.split('.')
+        var publisher = args[0]
+        var asset = args[1]
+        var varsion = process.execSync(`./build.sh ${adapter}`).toString().replace('\n', '')
+        package.adapters[adapter] = `https://ms-vscode.gallery.vsassets.io/_apis/public/gallery/publisher/${publisher}/extension/${asset}/${varsion}/assetbyname/Microsoft.VisualStudio.Services.VSIXPackage`
+    }
 }
 
 fs.writeFileSync('package.json', JSON.stringify(package, undefined, 4))
