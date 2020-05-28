c06992d1a1fa242b6bd9461b19a9208a96bb0379
MiaoWoo <admin@yumc.pw>
2020-05-28 10:37:42 +0800

    feat: auto create cdr config
    Signed-off-by: MiaoWoo <admin@yumc.pw>

--- vscode/entrypoint.sh
+++ vscode/entrypoint.sh
@@ -1,2 +1,10 @@
-code-server &
-tail -f ~/.config/code-server/config.yaml
+#!/bin/bash
+
+mkdir -p ~/.config/code-server
+cat > ~/.config/code-server/config.yaml<<EOF
+bind-addr: ${CDR_BIND_ADDR:-0.0.0.0:8080}
+auth: ${CDR_AUTH:-password}
+password: ${CDR_PASSWORD:-${RANDOM}${RANDOM}}
+cert: ${CDR_CERT:-false}
+EOF
+code-server
